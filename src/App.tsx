91756f7c7a101297b419bcbcefcd82b141bd5226
“Collin” <whalencollin@gmail.com>
2025-01-08 12:50:55 -0600

    App.tsx, clean up, CSS, & packages.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-import { useState } from "react";
-import { Text } from "@mantine/core";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "@infinite-table/infinite-react/index.css";
 import SearchPage from "./pages/SearchPage.tsx";
+import DetailsPage from "./pages/DetailsPage.tsx";
 import "./App.css";
 
 function App() {
   return (
-    <>
-      <Text>KorTerra Front End Coding Challenge </Text>
-      <SearchPage />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<SearchPage />} />
+        <Route path="/repo-details/:repoId" element={<DetailsPage />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,9 @@ import App from "./App.tsx";
 import { MantineProvider } from "@mantine/core";
 import '@mantine/core/styles.css';
 
-
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <MantineProvider
-      theme={{
-        colorScheme: "dark",
-      }}
       withGlobalStyles
       withNormalizeCSS
     >
